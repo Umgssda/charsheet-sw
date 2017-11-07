052e009eb2f1dcc034c93a0e214f0ccadd96c0d2
Umgssda <umgssda@gmail.com>
2017-11-07 08:06:23 +0100

    Initial Setup with basic sheet-component, grid-component and tile-component. Made Hexagons out of the tiles.

---
+++ clone_one.js
@@ -0,0 +1,138 @@
+var clone_one = {
+    "character": {
+        "name": "Clone One",
+        "traits": [
+            {
+                "name": "Geschick",
+                "value": "10",
+                "skills": [
+                    {
+                        "name": "Schusswaffen",
+                        "value": "10",
+                        "specialisations": [
+                            "Einhandwaffen"
+                        ]
+                    },
+                    {
+                        "name": "Nahkampf",
+                        "value": "8",
+                        "specialisations": [
+                            "Klingen",
+                            "Waffenlos"
+                        ]
+                    },
+                    {
+                        "name": "Heimlichkeit",
+                        "value": "6",
+                        "specialisations": [
+                            "Verbergen"
+                        ]
+                    },
+                    {
+                        "name": "Fahren",
+                        "value": "4",
+                        "specialisations": [
+                            "Motorräder"
+                        ]
+                    },
+                    {
+                        "name": "Sabotage",
+                        "value": "8",
+                        "specialisations": [
+                            "Schlösser knacken"
+                        ]
+                    },
+                    {
+                        "name": "Tanzen",
+                        "value": "6",
+                        "specialisations": [
+                            
+                        ]
+                    }
+                ]
+            },
+            {
+                "name": "Verstand",
+                "value": "4",
+                "skills": [
+                    {
+                        "name": "Wahrnehmung",
+                        "value": "4",
+                        "specialisations": [
+                            "Menschen"
+                        ]
+                    },
+                    {
+                        "name": "Straßenwissen",
+                        "value": "4",
+                        "specialisations": [
+                            "Gangs"
+                        ]
+                    },
+                    {
+                        "name": "Täuschung",
+                        "value": "4",
+                        "specialisations": [
+                            "Lügen"
+                        ]
+                    },{
+                        "name": "Basteln",
+                        "value": "4",
+                        "specialisations": [
+                            "Waffen & Rüstungen",
+                            "Mechanik"
+                        ]
+                    }
+                ]
+            },
+            {
+                "name": "Wille",
+                "value": "6",
+                "skills": [
+                    {
+                        "name": "Einschüchtern",
+                        "value": "6",
+                        "specialisations": [
+                            "Spontan"
+                        ]
+                    }
+                ]
+            },
+            {
+                "name": "Stärke",
+                "value": "8",
+                "skills": [
+                    {
+                        "name": "Projektile",
+                        "value": "6",
+                        "specialisations": [
+                            "Wurfwaffen",
+                            "Granaten"
+                        ]
+                    },
+                    {
+                        "name": "Athletik",
+                        "value": "6",
+                        "specialisations": [
+                            "Parcours",
+                            "Schwimmen"
+                        ]
+                    }
+                ]
+            },
+            {
+                "name": "Konstitution",
+                "value": "8"
+            }
+        ],
+
+        "edges": [
+            "Kräftig",
+            "Beidhändig",
+            "Nerven aus Stahl",
+            "Akrobatik",
+            "Kampfreflexe",
+            "Martial Artist"
+        ]
+    }
+}
